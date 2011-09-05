eda9222f3797b26e7033b4f83979e0d2b687ac56
HPNeo <hpneo@hotmail.com>
2011-09-04 20:09:54 -0500

    Actualizado seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+cat_1 = Category.create(:name => 'Restaurantes')
+cat_2 = Category.create(:name => 'Diversion')
+
+Category.create(:parent_id => cat_1.id, :name => 'Pollerias')
+Category.create(:parent_id => cat_1.id, :name => 'Chifas')
+Category.create(:parent_id => cat_1.id, :name => 'Carnes a la parrilla')
+Category.create(:parent_id => cat_1.id, :name => 'Pescados y mariscos')
+Category.create(:parent_id => cat_1.id, :name => 'Comida Criolla')
+Category.create(:parent_id => cat_1.id, :name => 'Cafes')
+
+Category.create(:parent_id => cat_2.id, :name => 'Discotecas')
+Category.create(:parent_id => cat_2.id, :name => 'Karaokes')
+Category.create(:parent_id => cat_2.id, :name => 'Pubs')
+Category.create(:parent_id => cat_2.id, :name => 'Cines')
+Category.create(:parent_id => cat_2.id, :name => 'Lounges')
